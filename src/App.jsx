900d68d55d6ab43269975430dad13aa9354f2bd0
Prasugupta21 <101421981+Prasugupta21@users.noreply.github.com>
2025-01-01 12:49:43 +0530

    App.jsx file updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import HomePage from './components/HomePage';
+import AddTaskPage from './components/AddTaskPage';
+import EditTaskPage from './components/EditTaskPage';
 
 function App() {
-  return(
- <>
-    </>
-  )
+  return (
+    <Router>
+      <Header />
+      <div className="container mx-auto">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/add-task" element={<AddTaskPage />} />
+          <Route path="/edit-task/:id" element={<EditTaskPage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
